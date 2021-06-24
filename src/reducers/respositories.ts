1111ceed12c1b9d725ca107275954a0bd656b140
mazarag2 <mazarag2@gmail.com>
2021-06-23 17:35:49 -0700

    fix typo

--- src/reducers/respositories.ts
+++ src/reducers/respositories.ts
@@ -28,7 +28,7 @@ type Action =
 enum ActionType {
     SEARCH_REPO = 'search_repo',
     SEARCH_REPO_SUCCESS = 'search_repo_success',
-    SEARCH_REPO_ERROR = 'search_repo_error''
+    SEARCH_REPO_ERROR = 'search_repo_error'
 }
 
 
